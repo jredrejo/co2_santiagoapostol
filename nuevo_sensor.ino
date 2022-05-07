61e312f710ac93b387a4c9140863e62e6adc8e79
Daniel <danielmunozamaya1@gmail.com>
2022-05-07 12:26:48 +0200

    Pedimos temperatura, raw co2 y background co2

--- nuevo_sensor.ino
+++ nuevo_sensor.ino
@@ -5,6 +5,7 @@
 
 #define CO2_MEASURE_PERIOD_MS 1000
 #define CO2_CALIBRATION_OFFSET 20
+#define SERIAL_BAUD_RATE 9600
 
 const char* ssid = "";
 const char* password = "";
@@ -12,13 +13,17 @@ const char* mqtt_server = "";
 
 MHZ19 co2;
 int last_co2_value = 0;
+float last_temperature_value = 0.0;
+unsigned int last_raw_co2_value = 0;
+int last_background_co2_value = 0;
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 char msg[50];
 
 void setup() {
 
-  Serial.begin(9600);
+  Serial.begin(SERIAL_BAUD_RATE);
   co2.begin(Serial);
   // co2.autoCalibration(true);
 
@@ -55,11 +60,27 @@ void loop() {
   client.loop();
 
   if( millis() - lastCo2Measure > CO2_MEASURE_PERIOD_MS){
+
+    // Obtención de datos
     last_co2_value = co2.getCO2();
     last_co2_value = last_co2_value + CO2_CALIBRATION_OFFSET;
+    last_temperature_value = co2.getTemperature();
+    last_raw_co2_value = co2.getCO2Raw();
+    last_background_co2_value = co2.getBackgroundCO2();
+    
     // Serial.println(last_co2_value);
+
+    // Publicación de datos mediante MQTT
     snprintf (msg, 50, "%d", last_co2_value);
     client.publish("instituto/clase/co2", msg);
+    snprintf (msg, 50, "%f", last_temperature_value);
+    client.publish("instituto/clase/temperatura", msg);
+    snprintf (msg, 50, "%d", last_raw_co2_value);
+    client.publish("instituto/clase/rawco2", msg);
+    snprintf (msg, 50, "%d", last_background_co2_value);
+    client.publish("instituto/clase/backgroundco2", msg);
+
+    
     lastCo2Measure = millis();
   }
 
